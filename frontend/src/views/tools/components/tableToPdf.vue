d24083d3a1bba49939abfe12012263efdfcd9588
zyxazhang <17379744014@163.com>
2022-05-10 18:16:58 +0800

    monir 添加合并行列

--- frontend/src/views/tools/components/tableToPdf.vue
+++ frontend/src/views/tools/components/tableToPdf.vue
@@ -1,27 +1,33 @@
 <template>
     <div class="t-pdf">
-        <a-button type="primary" @click="handleClickAdd('row')">
-            添加一行
+        <a-button type="primary" style="margin-left: 10px" @click="handleClickAdd('row')">
+            添加行
         </a-button>
         <a-button type="primary" @click="handleClickAdd('column')">
-            添加一列
+            添加列
+        </a-button>
+        <a-button type="primary" @click="handleClickMerge('row')">
+            合并行
+        </a-button>
+        <a-button type="primary" @click="handleClickMerge('column')">
+            合并列
         </a-button>
         <a-button type="primary">
             配置项
         </a-button>
-        <div class="t-table">
-            <table border="1" :bgcolor="bgColor">
+        <div class="t-content">
+            <div class="t-table">
                 <tr>
-                    <th class="tr-head" v-for="(item, index) in formHeadData" :key="index">
+                    <th class="tr-head" v-for="(item, index) in formHeadData" :key="index" :colspan="1" :rowspan="1">
                         {{ item.key }}
                     </th>
                 </tr>
                 <tr class="tr-body" v-for="(item, index) in trbody" :key="index">
-                    <td v-for="(item1, index1) in item.columns" :key="index1">{{ item1.name }}</td>
+                    <td v-for="(item1, index1) in item.columns" :key="index1" :colspan="item1.colspan" :rowspan="item1.rowspan">{{ item1.name }}</td>
                 </tr>
-            </table>
+            </div>
         </div>
-        <a-modal v-model="addColumnData.addColumnsvisible" title="添加列" @ok="handleColumnOk">
+        <a-modal v-model="addColumnData.addColumnsvisible" title="添加列" @ok="handleColumnAddOk">
             <a-form-model :model="addColumnData.config" :label-col="labelCol" :wrapper-col="wrapperCol">
                 <a-form-model-item label="列名称">
                     <a-input v-model="addColumnData.config.new_col_name" />
@@ -44,6 +50,34 @@
                     </div>
                 </a-form-model-item>
             </a-form-model>
+        </a-modal>
+        <a-modal v-model="mergeVisible" title="添加列" @ok="handleColumnMergeOk">
+            <template v-if="mergeType === 'row'">
+                <a-form-model :model="merge_row" :label-col="labelCol" :wrapper-col="wrapperCol">
+                    <a-form-model-item label="目标列">
+                        <a-input v-model="merge_row.target_col" />
+                    </a-form-model-item>
+                    <a-form-model-item label="目标开始行">
+                        <a-input v-model="merge_row.start_row" />
+                    </a-form-model-item>
+                    <a-form-model-item label="目标结束行">
+                        <a-input v-model="merge_row.end_row" />
+                    </a-form-model-item>
+                </a-form-model>
+            </template>
+            <template v-else>
+                <a-form-model :model="merge_col" :label-col="labelCol" :wrapper-col="wrapperCol">
+                    <a-form-model-item label="目标列">
+                        <a-input v-model="merge_col.target_row" />
+                    </a-form-model-item>
+                    <a-form-model-item label="目标开始行">
+                        <a-input v-model="merge_col.start_col" />
+                    </a-form-model-item>
+                    <a-form-model-item label="目标结束行">
+                        <a-input v-model="merge_col.end_col" />
+                    </a-form-model-item>
+                </a-form-model>
+            </template>
         </a-modal>
     </div>
 </template>
@@ -70,11 +104,15 @@ export default {
                     columns: [
                         {
                             name: '11',
-                            key: '1-11'
+                            key: '1-11',
+                            colspan: 1,
+                            rowspan: 1
                         },
-                         {
+                        {
                             name: '22',
-                            key: '1-22'
+                            key: '1-22',
+                            colspan: 1,
+                            rowspan: 1
                         }
                     ]
                 },
@@ -84,11 +122,15 @@ export default {
                     columns: [
                         {
                             name: '11eqwr',
-                            key: '2-11'
+                            key: '2-11',
+                            colspan: 1,
+                            rowspan: 1
                         },
-                         {
+                        {
                             name: '22',
-                            key: '2-22'
+                            key: '2-22',
+                            colspan: 1,
+                            rowspan: 1
                         }
                     ]
                 }
@@ -115,7 +157,21 @@ export default {
             },
             bgColor: '',
             labelCol: { span: 5 },
-            wrapperCol: { span: 14 }
+            wrapperCol: { span: 14 },
+            // 合并行
+            mergeVisible: false,
+            mergeType: '',
+            merge_row: {
+                target_col: 1,
+                start_row: '1',
+                end_row: '2'
+            },
+            // 合并列
+            merge_col: {
+                target_row: '',
+                start_col: '',
+                end_col: ''
+            }
         }
     },
     methods: {
@@ -149,7 +205,11 @@ export default {
                 this.addColumnData.config.listData = columns
             }
         },
-        handleColumnOk () {
+        handleClickMerge (type) {
+            this.mergeVisible = true
+            this.mergeType = type
+        },
+        handleColumnAddOk () {
             this.formHeadData.push({
                 key: this.addColumnData.config.new_col_name,
                 value: 'dqweqw'
@@ -157,10 +217,22 @@ export default {
             this.addColumnData.config.listData.forEach((item,index) => {
                 this.trbody[index].columns.push({
                     name: item.value,
-                    key: item.name
+                    key: item.name,
+                    rowspan: 1,
+                    colspan: 1
                 })
             })
             this.addColumnData.addColumnsvisible = false
+        },
+        handleColumnMergeOk () {
+            if (this.mergeType === 'row') {
+                console.log('合并行')
+                const { target_col, start_row, end_row } = this.merge_row
+                // 把rowspan 设置 为  end - start
+                // 把对应row 的 target_col 删除
+            } else {
+                console.log('合并列');
+            }
         }
     }
 }
@@ -171,23 +243,35 @@ export default {
     width: 100%;
     height: 800px;
     padding: 5px;
-    .t-table {
-        width: 100%;
+    .t-content {
+        min-width: 200px;
         height: 100%;
-        padding: 10px;
-        text-align: center;
-        .tr-head {
-            height: 60px;
-            width: 100px;
-            background-color: #eee;
+        .t-table {
+            // width: 100%;
+            margin: 10px;
+            display: inline-block;
             text-align: center;
-        }
-        .tr-body {
-            height: 60px;
-            // width: 100px;
-            td {
+            border: 1px solid #d9d9d9;
+            background-color: #d9d9d9;
+            box-sizing: border-box;
+            .tr-head {
+                height: 60px;
+                line-height: 60px;
                 width: 100px;
-                padding: 2px;
+                background-color: #eee;
+                text-align: center;
+                border: 1px solid #d9d9d9;
+            }
+            .tr-body {
+                height: 60px;
+                line-height: 60px;
+                // width: 100px;
+                td {
+                    width: 100px;
+                    padding: 2px;
+                    background-color: #fff;
+                    border: 1px solid #d9d9d9;
+                }
             }
         }
     }
